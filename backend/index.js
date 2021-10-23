6cac4d3bae0e44fafb254fe850b94e606a828ccb
syahrul mubarok <mubaroksyahrul8132gmail.com>
2021-10-23 17:37:46 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import db from './config/database.js';
 
 const app = express();
+
+try {
+  await db.authenticate();
+  console.log(' Database connected...');
+} catch (error) {
+  console.error('Connection error:', error);
+}
 
 app.get('/', (req, res) => {
   res.send('welcome');
